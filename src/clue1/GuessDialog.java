37b65af47daddd40a23dcc9b195f0c53c886122a
unknown <rbrining@lb266-29.adit.mines.edu>
2014-04-10 22:19:45 -0600

    It displayes the accusation box for human

---
+++ src/clue1/GuessDialog.java
@@ -0,0 +1,53 @@
+package clue1;
+
+import java.awt.Button;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+import clue1.DetectiveNotesDialog.CheckboxPanel;
+
+public class GuessDialog extends JDialog {
+	
+	private JComboBox<String> playerCombobox, weaponCombobox, roomCombobox;
+	
+	public GuessDialog(ArrayList<Card> deck, ClueGame game, String room) {
+		setTitle("Make Suggestion");
+		setSize(500, 500);
+		
+		setLayout(new GridLayout(4,2));
+		playerCombobox = new JComboBox<String>();
+		weaponCombobox = new JComboBox<String>();
+		roomCombobox = new JComboBox<String>();
+		//create comboboxes based on cards in deck
+		for (Card x : deck) { 
+			if (x.getCardType().equals(Card.CardType.PLAYER)) {
+				playerCombobox.addItem(x.getName()); 
+			}
+			if (x.getCardType().equals(Card.CardType.WEAPON)) {
+				weaponCombobox.addItem(x.getName()); 
+			}
+			if (x.getCardType().equals(Card.CardType.ROOM)) {
+				roomCombobox.addItem(x.getName()); 
+			}
+		}
+		
+		add(new Label("Your Room"));
+		add((roomCombobox));
+		add(new Label("Person"));
+		add(playerCombobox);
+		add(new Label("Weapon"));
+		add(weaponCombobox);
+		JButton submit = new JButton("ok");
+		add(submit);
+		add(new Button("Cancel"));
+		
+	}
+}
